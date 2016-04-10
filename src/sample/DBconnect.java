a0d355ef4e8274fa4f9f948666a98d7882dbf138
Yeupho <hotaznbuns@gmail.com>
2016-04-09 21:45:00 -0500

    Working Connections with localhost

---
+++ src/sample/DBconnect.java
@@ -0,0 +1,37 @@
+package sample;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ * 
+ * @author Narayan
+ */
+
+public class DBconnect {
+
+    private static Connection conn;
+    private static String url = "jdbc:sqlserver://localhost\\SQLEXPRESS;databaseName=DulceDatabase;integratedSecurity=true;";
+
+
+    public static Connection connect() throws SQLException{
+        try{
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver()").newInstance();
+        }catch(ClassNotFoundException cnfe){
+            System.err.println("Error: "+cnfe.getMessage());
+        }catch(InstantiationException ie){
+            System.err.println("Error: "+ie.getMessage());
+        }catch(IllegalAccessException iae){
+            System.err.println("Error: "+iae.getMessage());
+        }
+
+        conn = DriverManager.getConnection(url);
+        return conn;
+    }
+    public static Connection getConnection() throws SQLException, ClassNotFoundException{
+        if(conn !=null && !conn.isClosed())
+            return conn;
+        connect();
+        return conn;
+
+    }
+}
